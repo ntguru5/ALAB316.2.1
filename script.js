92e14efb9c156203978c9403dda588725fa8af53
Ken Yeh <yehken@gmail.com>
2024-09-25 21:52:11 -0700

    add pseudocode

---
+++ script.js
@@ -0,0 +1,15 @@
+/** PSEUDOCODE
+ * When user clicks Start Game button,
+ * generate a random number
+ * set initial values for attempts left
+ * min range and max range 1 -100
+ * While player has attempts > 0,
+ * Display message asking player to guess between min and max range
+ * convert input into player guess variable
+ * if player guess matches, display success message
+ * Calculate diff between guess and correct number
+ * Create messages that give hints to player (too high, too low, very close)
+ * Decrease number of attempts left, if not attempts left game is over
+ * Display if player wins or loses at end
+ * At end, allow player to start new game or quit
+ */
